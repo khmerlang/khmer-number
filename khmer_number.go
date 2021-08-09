d8de8475636e57018be83b2d4419c3d7503f1067
Sreang Rathanak <sreangrathanak@yahoo.com>
2021-08-09 08:11:12 +0700

    Update comlex logic

--- khmer_number.go
+++ khmer_number.go
@@ -32,28 +32,27 @@ var KHMER_TENTH = []string{
   "កៅសិប",
 }
 
-func int2Word(num int64) string {
+func int2Word(num int64, space string) string {
   if num == 0 {
     return "សូន្យ"
   } else {
-    return int2Khmer(num)
+    return int2Khmer(num, space)
   }
 }
 
-func int2Khmer(num int64) string {
+func int2Khmer(num int64, space string) string {
   if num >= 1000000 {
-    return fmt.Sprintf("%sលាន%s", int2Khmer(num / 1000000), int2Khmer(num % 1000000))
-
+    return fmt.Sprintf("%sលាន%s%s", int2Khmer(num / 1000000, space), space, int2Khmer(num % 1000000, space))
   } else if num >= 100000 {
-    return fmt.Sprintf("%sសែន%s", int2Khmer(num / 100000), int2Khmer(num % 100000))
+    return fmt.Sprintf("%sសែន%s%s", int2Khmer(num / 100000, space), space, int2Khmer(num % 100000, space))
   } else if num >= 10000 {
-    return fmt.Sprintf("%sម៉ឺន%s", int2Khmer(num / 10000), int2Khmer(num % 10000))
+    return fmt.Sprintf("%sម៉ឺន%s%s", int2Khmer(num / 10000, space), space, int2Khmer(num % 10000, space))
   } else if num >= 1000 {
-    return fmt.Sprintf("%sពាន់%s", int2Khmer(num / 1000), int2Khmer(num % 1000))
+    return fmt.Sprintf("%sពាន់%s%s", int2Khmer(num / 1000, space), space,  int2Khmer(num % 1000, space))
   } else if num >= 100 {
-    return fmt.Sprintf("%sរយ%s", int2Khmer(num / 100), int2Khmer(num % 100))
+    return fmt.Sprintf("%sរយ%s%s", int2Khmer(num / 100, space), space,  int2Khmer(num % 100, space))
   } else if num >= 10 {
-    return fmt.Sprintf("%s%s", KHMER_TENTH[num / 10], KHMER_DIGIT_WORD[num % 10])
+    return fmt.Sprintf("%s%s%s", KHMER_TENTH[num / 10], space, KHMER_DIGIT_WORD[num % 10])
   } else if num > 0 {
     return KHMER_DIGIT_WORD[num];
   }
@@ -73,11 +72,11 @@ func countLeading(str string, item rune) int {
    return counter
  }
 
-func Num2Word(num string) string {
+func Num2Word(num string, space string) string {
   nums := strings.Split(num, ".")
   if len(nums) == 1 {
     digit, _ := strconv.ParseInt(nums[0], 10, 64)
-    return int2Word(digit)
+    return int2Word(digit, space)
   }
 
   digit, _ := strconv.ParseInt(nums[0], 10, 64)
@@ -85,10 +84,11 @@ func Num2Word(num string) string {
   lead_zero := ""
   for _, value := range nums[1] {
     if string(value) == "0" {
+      lead_zero += space
       lead_zero += "សូន្យ"
     } else {
       break
     }
    }
-  return fmt.Sprintf("%sចុច%s%s", int2Word(digit), lead_zero, int2Khmer(precision))
+  return fmt.Sprintf("%s%sចុច%s%s%s", int2Word(digit, space), space, lead_zero, space, int2Khmer(precision, space))
 }
--- khmer_number_test.go
+++ khmer_number_test.go
@@ -5,13 +5,20 @@ import (
 )
 
 func TestingNum2Word(num string, word string, t *testing.T) {
-  result := Num2Word(num)
+  result := Num2Word(num, "")
   if result != word {
     t.Errorf("(%s) expect = (%s), got (%s)", num, word, result)
   }
 }
 
-func TestConvertWordToRoman(t *testing.T) {
+func TestingNum2WordWithSpace(num string, word string, t *testing.T) {
+  result := Num2Word(num, " ")
+  if result != word {
+    t.Errorf("(%s) expect = (%s), got (%s)", num, word, result)
+  }
+}
+
+func TestConvertNumToWord(t *testing.T) {
   TestingNum2Word("0", "សូន្យ", t)
   TestingNum2Word("1", "មួយ", t)
   TestingNum2Word("2", "ពីរ", t)
@@ -42,4 +49,37 @@ func TestConvertWordToRoman(t *testing.T) {
   TestingNum2Word("12.05", "ដប់ពីរចុចសូន្យប្រាំ", t)
   TestingNum2Word("12.0005", "ដប់ពីរចុចសូន្យសូន្យសូន្យប្រាំ", t)
   TestingNum2Word("12.0000", "ដប់ពីរចុចសូន្យសូន្យសូន្យសូន្យ", t)
+}
+
+func TestConvertNumToWordWithSpace(t *testing.T) {
+  TestingNum2WordWithSpace("0", "សូន្យ", t)
+  TestingNum2WordWithSpace("1", "មួយ", t)
+  TestingNum2WordWithSpace("2", "ពីរ", t)
+  TestingNum2WordWithSpace("3", "បី", t)
+  TestingNum2WordWithSpace("4", "បួន", t)
+  TestingNum2WordWithSpace("5", "ប្រាំ", t)
+  TestingNum2WordWithSpace("6", "ប្រាំមួយ", t)
+  TestingNum2WordWithSpace("7", "ប្រាំពីរ", t)
+  TestingNum2WordWithSpace("8", "ប្រាំបី", t)
+  TestingNum2WordWithSpace("9", "ប្រាំបួន", t)
+  TestingNum2WordWithSpace("10", "ដប់ ", t)
+  TestingNum2WordWithSpace("20", "ម្ភៃ ", t)
+  TestingNum2WordWithSpace("30", "សាមសិប ", t)
+  TestingNum2WordWithSpace("40", "សែសិប ", t)
+  TestingNum2WordWithSpace("50", "ហាសិប ", t)
+  TestingNum2WordWithSpace("60", "ហុកសិប ", t)
+  TestingNum2WordWithSpace("70", "ចិតសិប ", t)
+  TestingNum2WordWithSpace("80", "ប៉ែតសិប ", t)
+  TestingNum2WordWithSpace("90", "កៅសិប ", t)
+  TestingNum2WordWithSpace("100", "មួយរយ ", t)
+  TestingNum2WordWithSpace("1000", "មួយពាន់ ", t)
+  TestingNum2WordWithSpace("10000", "មួយម៉ឺន ", t)
+  TestingNum2WordWithSpace("100000", "មួយសែន ", t)
+  TestingNum2WordWithSpace("1000000", "មួយលាន ", t)
+  TestingNum2WordWithSpace("1234", "មួយពាន់ ពីររយ សាមសិប បួន", t)
+  TestingNum2WordWithSpace("1234.78", "មួយពាន់ ពីររយ សាមសិប បួន ចុច ចិតសិប ប្រាំបី", t)
+  TestingNum2WordWithSpace("10005.55", "មួយម៉ឺន ប្រាំ ចុច ហាសិប ប្រាំ", t)
+  TestingNum2WordWithSpace("12.05", "ដប់ ពីរ ចុច សូន្យ ប្រាំ", t)
+  TestingNum2WordWithSpace("12.0005", "ដប់ ពីរ ចុច សូន្យ សូន្យ សូន្យ ប្រាំ", t)
+  TestingNum2WordWithSpace("12.0000", "ដប់ ពីរ ចុច សូន្យ សូន្យ សូន្យ សូន្យ ", t)
 }
